a8361a317304fc38bc759059301d16b614cef3c4
Limah-T <limahtechnology@yahoo.com>
2025-08-31 15:49:24 +0100

    Cache Property List View

--- properties/views.py
+++ properties/views.py
@@ -5,4 +5,4 @@ from .models import Property  # assuming you have a Property model
 @cache_page(60 * 15)  # cache for 15 minutes
 def property_list(request):
     properties = Property.objects.all().values("id", "name", "price", "location")  
-    return JsonResponse(list(properties), safe=False)
+    return JsonResponse(data=list(properties), safe=False)

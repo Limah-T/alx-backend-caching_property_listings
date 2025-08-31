9ab75b750384c957b7ffcba9a33efb350be32117
Limah-T <limahtechnology@yahoo.com>
2025-08-31 15:48:36 +0100

    Cache Property List View

--- properties/views.py
+++ properties/views.py
@@ -1,12 +1,8 @@
-from .models import Property
-from rest_framework.viewsets import ModelViewSet
 from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
-class PropertyView(ModelViewSet):
-    queryset = Property.objects.all()
+from .models import Property  # assuming you have a Property model
 
-    @cache_page(60 * 15)
-    def list(self, request, *args, **kwargs):
-        properties = Property.objects.all().values("id", "name", "price", "location")  
-        return JsonResponse(list(properties), safe=False)
-
+@cache_page(60 * 15)  # cache for 15 minutes
+def property_list(request):
+    properties = Property.objects.all().values("id", "name", "price", "location")  
+    return JsonResponse(list(properties), safe=False)
